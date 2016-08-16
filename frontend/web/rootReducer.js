87ba57e8cafb7f117865d5fd6d062271978b5cfe
Jeff Lau <jeff-lau@live.com>
2016-08-16 14:36:03 +0800

    Removed unnecessary actions and reducers

--- frontend/web/rootReducer.js
+++ frontend/web/rootReducer.js
@@ -2,20 +2,12 @@
 import { combineReducers } from 'redux';
 import serverError from '../reducers/serverError';
 import config from '../reducers/config';
-import seasons from '../reducers/seasons';
-import animeCache from '../reducers/animeCache';
-import animeListOptions from '../reducers/animeListOptions';
-import years from '../reducers/years';
 import { routerReducer } from 'react-router-redux';
 import {reducer as formReducer} from 'redux-form';
 
 const rootReducer = combineReducers({
   serverError,
   config,
-  seasons,
-  animeCache,
-  animeListOptions,
-  years,
   routing: routerReducer,
   form: formReducer
 })
